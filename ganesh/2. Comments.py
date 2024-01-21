0deff05828c53e3acd5f6ea49098737eed4363b0
koppulaganesh <156056853+koppulaganesh@users.noreply.github.com>
2024-01-21 16:59:31 +0530

    Delete ganesh/2. Comments.py

--- ganesh/2. Comments.py
+++
@@ -1,9 +0,0 @@
-#Comment in python:-
-'''
-1. Single Line Comment: in which we use hashtag (#) 
-2. Multiline comment: in which we use tripple quotes ('''''')
-Note: 
-a) comments are ignored by the compiler and interpreter.
-b) Comments are mainly usefull for the simple understanding of the complex code.
-
-'''
